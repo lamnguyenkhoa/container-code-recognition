da60ce60d4598426ada8a2f11905d56556adce7d
LAM NGUYEN KHOA <khoalamvn@gmail.com>
2021-07-05 20:34:31 +0700

    Added new options for pytesseract config

--- ocr.py
+++ ocr.py
@@ -1,10 +1,16 @@
 import pytesseract
 
 
-def build_tesseract_options(is_backcode):
+def build_tesseract_options(is_sidecode=False):
     # tell Tesseract to only OCR alphanumeric characters
     alphanumeric = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
-    options = "-c tessedit_char_whitelist={}".format(alphanumeric)
+    if is_sidecode:
+        options = "--psm 6"  # Assume a single uniform block of text
+    else:
+        options = "--psm 3"  # Fully automatic page segmentation, but no OSD. (Default)
+    options += " --oem 3"
+    options += " -c tessedit_char_whitelist={}".format(alphanumeric)
+    options += " load_freq_dawg=false load_system_dawg=false"  # Tesseract won't use dictionary
     # set the PSM mode
     # options += " --psm {}".format(7)
     # return the built options string
@@ -20,8 +26,12 @@ def reformat(original_code):
     ...
 
 
-def find_code_in_image(img, is_backcode=True):
-    options = build_tesseract_options(is_backcode)
+def find_code_in_image(img):
+    im_h, im_w = img.shape[0:2]
+    if im_h > im_w:  # If this image is a side code image
+        options = build_tesseract_options(True)
+    else:
+        options = build_tesseract_options(False)
     result = pytesseract.image_to_string(img, config=options)
     return result
 
