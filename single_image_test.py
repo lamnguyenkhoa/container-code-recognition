9f7bb50e60d5e2e285161a78b4a059c7b81d44a8
LAM NGUYEN KHOA <khoalamvn@gmail.com>
2021-07-07 22:38:09 +0700

    Minor fix

--- single_image_test.py
+++ single_image_test.py
@@ -10,19 +10,27 @@ def get_code_in_image():
     """
 
 
+def resize_to_suitable(src_img):
+    im_h, im_w = src_img.shape[0:2]
+    resized_img = cv2.resize(src_img, 2)
+    return resized_img
+
+
 def main():
     """
     Test recognition ability on a single image. Only used for testing
     """
-    src_img = cv2.imread("images/code1.png")
+    src_img = cv2.imread("images/ocr_test.png")
     clean_img = image_processing.cleanup_backcode_image(src_img)
     clean_img = cv2.bitwise_not(clean_img)
     clean_img = cv2.blur(clean_img, (2, 2))
     res = ocr.find_code_in_image(clean_img)
-    print(clean_img.shape)
     print(res)
     display_image_cv2(clean_img, "Final result")
 
 
 if __name__ == "__main__":
     main()
+
+# TODO: Find a wat to deskew vertical code
+# TODO: But for now, focus on trying to make it run just based on back code
