9b77541fe25269cfa63b030969750c337819b939
LAM NGUYEN KHOA <khoalamvn@gmail.com>
2021-07-07 22:37:46 +0700

    Added reformat function

--- ocr.py
+++ ocr.py
@@ -21,9 +21,26 @@ def error_check(original_code):
     ...
 
 
-def reformat(original_code):
+def reformat_code(original_code):
     """ Reformat the text into better format"""
-    ...
+    formatted_code = None
+    n = len(original_code)
+    if n <= 20:
+        # Likely to be backcode
+        formatted_code = original_code[0:n-1]  # Remove weird last character
+        formatted_code = formatted_code.replace(" ", "")  # Remove whitespace characters
+        formatted_code = formatted_code.replace("\n", "")  # Remove whitespace character
+    else:
+        # Side code, very prone to error
+        tmp = original_code[0:n-1]  # Remove weird last character
+        tmp = tmp.replace(" ", "")  # Remove whitespace characters
+        tmp = tmp.replace("\n", "")  # Remove whitespace character
+        owner_code = tmp[0]+tmp[2]+tmp[4]+tmp[6]
+        iso_code = tmp[1]+tmp[3]+tmp[5]+tmp[7]
+        serial_number = tmp[8:-2]  # From 9th digit to 2nd last digit
+        check_digit = tmp[-1]  # Last digit
+        formatted_code = owner_code + serial_number + check_digit + iso_code
+    return formatted_code
 
 
 def find_code_in_image(img):
@@ -33,6 +50,7 @@ def find_code_in_image(img):
     else:
         options = build_tesseract_options(False)
     result = pytesseract.image_to_string(img, config=options)
+    result = reformat_code(result)
     return result
 
 
@@ -43,3 +61,4 @@ def main():
 if __name__ == '__main__':
     main()
 
+# TODO: Improve reformat for side code
