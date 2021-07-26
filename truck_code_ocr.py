e7b8c939bcd6f5906e0d7b6964a7e797573c42e6
NKLam <khoalamvn@gmail.com>
2021-07-26 16:07:23 +0700

    Show code instead of just bounding box

--- truck_code_ocr.py
+++ truck_code_ocr.py
@@ -1,5 +1,7 @@
 import cv2
 import argparse
+
+from single_frame_process import get_code_and_draw
 from src.code_region_detector import build_model, detect, draw_all_bounding_boxes
 
 
@@ -19,22 +21,23 @@ def main(args):
     if not cap.isOpened():
         print("truck_code_ocr/main(): Error opening video stream or file")
 
-    # Read until video is completed
-    cv2.namedWindow('Output frame', cv2.WINDOW_NORMAL)
-    cv2.resizeWindow('Output frame', 1500, 800)
-    count = 0
+    # Load the model
     net, classes, output_layers = build_model(args['classes'], args['weights'], args['config'])
     net.setPreferableBackend(cv2.dnn.DNN_BACKEND_CUDA)
     net.setPreferableTarget(cv2.dnn.DNN_TARGET_CUDA)
+    # Read until video is completed
+    cv2.namedWindow('Output frame', cv2.WINDOW_NORMAL)
+    cv2.resizeWindow('Output frame', 1280, 720)
+    count = 0
     while cap.isOpened():
         success, frame = cap.read()
         if success:
-            # Only take every 2th frame
+            # Only take every 2nd frame
             if count % 2 == 0:
                 # Detect code and recognize it here
                 class_ids, boxes, confidences = detect(net, frame, output_layers)
-                draw_all_bounding_boxes(frame, boxes, class_ids, confidences, classes)
-                # frame, codes = get_code_and_draw(frame, class_ids, classes, boxes)
+                # draw_all_bounding_boxes(frame, boxes, class_ids, confidences, classes)
+                frame, codes = get_code_and_draw(frame, class_ids, classes, boxes)
                 cv2.imshow("Output frame", frame)
 
             # Press Q on keyboard to  exit
